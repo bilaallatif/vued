6b14efb45714ef7c5995a795a6adb27551337a08
bilaallatif <latif.bilaal@outlook.com>
2025-07-27 00:44:26 +0100

    Implemented tsoa config programmatically

---
+++ apps/backend/tsoa-config.ts
@@ -0,0 +1,31 @@
+import {
+  generateRoutes,
+  generateSpec,
+  ExtendedRoutesConfig,
+  ExtendedSpecConfig,
+} from "tsoa";
+import config from "./src/config/config";
+
+(async () => {
+  const specOptions: ExtendedSpecConfig = {
+    basePath: "",
+    noImplicitAdditionalProperties: "throw-on-extras",
+    entryFile: "src/app.ts",
+    controllerPathGlobs: ["src/**/*-controller.ts"],
+    specVersion: 3,
+    outputDirectory: "dist",
+    host: `localhost:${config.port}`,
+    schemes: ["http"],
+  };
+
+  const routeOptions: ExtendedRoutesConfig = {
+    noImplicitAdditionalProperties: "throw-on-extras",
+    entryFile: "src/app.ts",
+    controllerPathGlobs: ["src/**/*-controller.ts"],
+    routesDir: "build",
+    bodyCoercion: false,
+  };
+
+  await generateSpec(specOptions);
+  await generateRoutes(routeOptions);
+})();
