4832f118e5bf7b7fd58dfeee2ac1028c6b4e0065
bilaallatif <latif.bilaal@outlook.com>
2025-08-12 21:52:12 +0100

    Added Tanstack Query to frontend

--- apps/frontend/src/components/login.tsx
+++ apps/frontend/src/components/login.tsx
@@ -1,8 +1,21 @@
 import { createRoute } from "@tanstack/react-router";
 import { BaseRoute } from "./base.tsx";
+import { Default } from "@vued/sdk/api";
+import { useQuery } from "@tanstack/react-query";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const { isPending, error, data } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () =>
+      await Default.login({
+        body: { username: "bilaal", password: "test" },
+      }),
+  });
+
+  if (isPending) return <h1>Loading...</h1>;
+  if (error) return <h1>Error: {error.message}</h1>;
+
+  return <h1>{data?.data}</h1>;
 };
 
 export const LoginRoute = createRoute({
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,12 +3,23 @@ import { createRoot } from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { BaseRoute } from "./components/base.tsx";
 import { LoginRoute } from "./components/login.tsx";
+import { client } from "@vued/sdk/api/client.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const routeTree = BaseRoute.addChildren([LoginRoute]);
 const router = createRouter({ routeTree });
 
+const apiUrl: string = import.meta.env.VITE_API_URL;
+client.setConfig({
+  baseUrl: apiUrl,
+});
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
