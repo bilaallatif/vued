7511f852e6ac18b80e80d691d83a02ea9eb36292
bilaallatif <latif.bilaal@outlook.com>
2025-08-24 23:44:37 +0100

    Disabling backlight for now

--- apps/frontend/src/components/login.tsx
+++ apps/frontend/src/components/login.tsx
@@ -9,15 +9,15 @@ const Login = () => {
   return (
     <>
       {/* Backlight */}
-      <div className="absolute w-full h-full -z-10 transform-gpu overflow-hidden blur-3xl">
-        <div
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-          className="relative w-full h-full bg-linear-to-tr from-yellow-900 to-yellow-200 opacity-10"
-        />
-      </div>
+      {/*<div className="absolute w-full h-full -z-10 transform-gpu overflow-hidden blur-3xl scale-80">*/}
+      {/*  <div*/}
+      {/*    style={{*/}
+      {/*      clipPath:*/}
+      {/*        "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",*/}
+      {/*    }}*/}
+      {/*    className="relative w-full h-full bg-linear-to-tr from-yellow-900 to-yellow-200 opacity-10"*/}
+      {/*  />*/}
+      {/*</div>*/}
 
       <div className={"flex flex-col gap-10 mx-auto"}>
         <div className={"flex flex-col gap-2"}>
