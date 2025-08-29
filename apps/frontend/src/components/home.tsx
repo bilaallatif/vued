7e1235e1b3966218e21e3bc1e0c960ebd0f23a67
bilaallatif <latif.bilaal@outlook.com>
2025-08-30 00:23:24 +0100

    Authenticated home

--- apps/frontend/src/components/home.tsx
+++ apps/frontend/src/components/home.tsx
@@ -2,7 +2,11 @@ import { createRoute } from "@tanstack/react-router";
 import { BaseRoute } from "./base.tsx";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className={"w-full h-full flex flex-col items-center justify-center"}>
+      <h1 className={"text-5xl text-neutral-400"}>Authenticated</h1>
+    </div>
+  );
 };
 
 export const HomeRoute = createRoute({
