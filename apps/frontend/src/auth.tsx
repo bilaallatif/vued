8ffdc061740ea0f84c66c953d9cec0f2bb1feae9
bilaallatif <latif.bilaal@outlook.com>
2025-09-01 23:26:33 +0100

    Got auth working (but no auto refresh)

--- apps/frontend/src/auth.tsx
+++ apps/frontend/src/auth.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
+import { client } from "@vued/sdk/api/client.gen";
 import { Default } from "@vued/sdk/api";
 
 export interface AuthState {
@@ -10,8 +11,20 @@ const AuthContext = createContext<AuthState | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [_accessToken, set_accessToken] = useState<string | null>(null);
+  const [accessToken, setAccessToken] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+
+  const apiUrl: string = import.meta.env.VITE_API_URL;
+
+  client.setConfig({
+    baseUrl: apiUrl,
+    credentials: "include",
+  });
+
+  client.interceptors.request.use((request) => {
+    request.headers.set("Authorization", `Bearer ${accessToken}`);
+    return request;
+  });
 
   // Attempt to refresh access_token on mount
   useEffect(() => {
@@ -19,7 +32,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       .then((new_access_token) => {
         if (new_access_token && new_access_token.data) {
           setIsAuthenticated(true);
-          set_accessToken(new_access_token.data);
+          setAccessToken(new_access_token.data);
           setIsLoading(false);
         }
       })
@@ -47,7 +60,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     });
     if (new_access_token && new_access_token.data) {
       setIsAuthenticated(true);
-      set_accessToken(new_access_token.data);
+      setAccessToken(new_access_token.data);
     }
   };
 
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,17 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { client } from "@vued/sdk/api/client.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import { AuthProvider } from "./auth.tsx";
 import { App } from "./app.tsx";
-
-const apiUrl: string = import.meta.env.VITE_API_URL;
-
-client.setConfig({
-  baseUrl: apiUrl,
-  credentials: "include",
-});
 
 const queryClient = new QueryClient();
 
