33fc4687bcb3750ca79b7054576afa4e68ef7c95
bilaallatif <latif.bilaal@outlook.com>
2025-09-04 20:00:43 +0100

    Fully working interceptor logic

--- apps/frontend/src/auth.tsx
+++ apps/frontend/src/auth.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useRef,
+} from "react";
 import { client } from "@vued/sdk/api/client.gen";
 import { Default } from "@vued/sdk/api";
 
@@ -14,38 +20,72 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [accessToken, setAccessToken] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
+  const access_token_ref = useRef<string | null>(null);
+  const setup_interceptors_ref = useRef(false);
+
   const apiUrl: string = import.meta.env.VITE_API_URL;
 
-  client.setConfig({
-    baseUrl: apiUrl,
-    credentials: "include",
-  });
+  let authInstanceId = 0;
 
-  client.interceptors.request.use((request) => {
-    request.headers.set("Authorization", `Bearer ${accessToken}`);
-    return request;
-  });
-
-  client.interceptors.response.use((res, req, _opts): Response => {
-    if (res.status == 401) {
-      Default.refresh().then((new_access_token) => {
-        if (new_access_token && new_access_token.data) {
-          console.log("Refreshing token!");
-          setAccessToken(new_access_token.data);
-          const retryReq = new Request(req);
-          retryReq.headers.set(
-            "Authorization",
-            `Bearer ${new_access_token.data}`,
-          );
-          return fetch(retryReq);
-        }
-      });
-    }
-    return res;
-  });
+  // Update the mutable instance of access_token when accessToken changes
+  useEffect(() => {
+    access_token_ref.current = accessToken;
+  }, [accessToken]);
 
   // Attempt to refresh access_token on mount
   useEffect(() => {
+    const id = ++authInstanceId;
+    console.log("Mounting with id:", id);
+
+    client.setConfig({
+      // Set base URL
+      baseUrl: apiUrl,
+      // Send/Receive cookies
+      credentials: "include",
+    });
+
+    // Only setup interceptors once
+    if (!setup_interceptors_ref.current) {
+      // Attach access_token to all requests
+      client.interceptors.request.use((request) => {
+        request.headers.set(
+          "Authorization",
+          `Bearer ${access_token_ref.current}`,
+        );
+        return request;
+      });
+
+      client.interceptors.response.use((res, req, _opts): Response => {
+        // If response is 401 and is not a retry request
+        if (
+          !req.url.endsWith("/refresh") &&
+          res.status == 401 &&
+          req.headers.get("Retry") != "true"
+        ) {
+          // Attempt to refresh access_token
+          Default.refresh()
+            .then((new_access_token) => {
+              if (new_access_token && new_access_token.data) {
+                console.log("Refreshing token!");
+                setAccessToken(new_access_token.data);
+                const retryReq = new Request(req);
+                retryReq.headers.set(
+                  "Authorization",
+                  `Bearer ${new_access_token.data}`,
+                );
+                retryReq.headers.set("Retry", "true");
+                return fetch(retryReq);
+              }
+            })
+            .catch(() => {
+              console.log("Failed refresh!");
+            });
+        }
+        return res;
+      });
+      setup_interceptors_ref.current = true;
+    }
+
     Default.refresh()
       .then((new_access_token) => {
         if (new_access_token && new_access_token.data) {
@@ -60,6 +100,9 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       .finally(() => {
         setIsLoading(false);
       });
+    return () => {
+      console.log("Cleaning up for id:", id);
+    };
   }, []);
 
   // Show loading state while checking auth
