05000e7f2e4a090f77f747f90a2d83b2c00e1183
bilaallatif <latif.bilaal@outlook.com>
2025-09-20 03:13:25 +0100

    Added blur backdrop on modal

--- apps/frontend/src/components/modal.tsx
+++ apps/frontend/src/components/modal.tsx
@@ -14,16 +14,22 @@ export const Modal = ({
   return (
     <div
       className={
-        "fixed h-4/5 flex flex-col items-center justify-start py-10 p-20 bg-neutral-800 rounded-md animate-fade-quick"
+        "fixed inset-0 bg-black/40 backdrop-blur-sm flex justify-center items-center"
       }
     >
-      <button
-        className={"absolute top-2 right-3 text-gray-500 hover:text-gray-700"}
-        onClick={onClose}
+      <div
+        className={
+          "fixed h-4/5 flex flex-col items-center justify-start py-10 p-20 bg-neutral-800 rounded-md animate-fade-quick"
+        }
       >
-        &#x2715; {/* Close button */}
-      </button>
-      {children}
+        <button
+          className={"absolute top-2 right-3 text-gray-500 hover:text-gray-700"}
+          onClick={onClose}
+        >
+          &#x2715; {/* Close button */}
+        </button>
+        {children}
+      </div>
     </div>
   );
 };
