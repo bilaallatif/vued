27e7837768a62a4a80bbfa05b3a556bb22b9ea3c
bilaallatif <latif.bilaal@outlook.com>
2025-09-20 01:40:09 +0100

    Added modal component

---
+++ apps/frontend/src/components/modal.tsx
@@ -0,0 +1,27 @@
+export const Modal = ({
+  isOpen,
+  onClose,
+  children,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}) => {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className={
+        "fixed inset-50 flex items-center justify-center bg-neutral-800 rounded-md animate-fade-quick"
+      }
+    >
+      <button
+        className={"absolute top-2 right-3 text-gray-500 hover:text-gray-700"}
+        onClick={onClose}
+      >
+        &#x2715; {/* Close button */}
+      </button>
+      {children}
+    </div>
+  );
+};
--- apps/frontend/src/routers/routers/authenticated_routers/home.tsx
+++ apps/frontend/src/routers/routers/authenticated_routers/home.tsx
@@ -1,10 +1,22 @@
 import { createRoute } from "@tanstack/react-router";
 import { AuthenticatedLayoutRoute } from "../authenticated_base.tsx";
+import { useState } from "react";
+import { Modal } from "../../../components/modal.tsx";
 
 const Home = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className={"w-full h-full flex flex-col items-center justify-center"}>
-      <h1 className={"text-5xl text-neutral-400"}>Home</h1>
+      <button
+        className={
+          "text-neutral-900 text-2xl bg-yellow-600 p-2 rounded-md hover:bg-yellow-500 hover:scale-110 transition-transform duration-200"
+        }
+        onClick={() => setIsModalOpen(true)}
+      >
+        New Review
+      </button>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
   );
 };
