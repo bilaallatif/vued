37730b6d28850a2bc2c1e70dd56b96c3272a780c
bilaallatif <bilaallatif@outlook.com>
2025-09-28 21:50:20 +0100

    Refactored transactions into test hooks

--- apps/backend/tests/integration/setup.ts
+++ apps/backend/tests/integration/setup.ts
@@ -1,14 +1,16 @@
-import { beforeAll, afterAll } from "vitest";
+import { beforeAll, afterAll, beforeEach, afterEach } from "vitest";
 import {
   PostgreSqlContainer,
   StartedPostgreSqlContainer,
 } from "@testcontainers/postgresql";
 import config from "../../src/config/config";
 import { execaCommand } from "execa";
+import { PrismaClient } from "@prisma/client";
+import { iocContainer } from "../../src/ioc";
+import { Rollback } from "../../src/types/exceptions";
 
 let db_container: StartedPostgreSqlContainer;
 
-// todo: should make this beforeAll s.t. we can instead use transactions
 beforeAll(async () => {
   // Spin up container for database
   db_container = await new PostgreSqlContainer("postgres:13.3-alpine")
@@ -34,6 +36,68 @@ beforeAll(async () => {
   // This should apply to app consuming module level config
   config.db_url = `postgresql://test:test@localhost:${mapped_port}/test`;
 }, 100000);
+
+let tx_runner: Promise<void>;
+let test_waiter: Promise<void>;
+let tx_close;
+
+beforeEach(async () => {
+  // Create lient to testcontainer
+  const p_client: PrismaClient = new PrismaClient({
+    datasourceUrl: config.db_url,
+  });
+
+  // Create promise to hold transaction until test is done
+  const tx_waiter = new Promise<void>((resolve) => {
+    // Globally set resolver
+    tx_close = resolve;
+  });
+
+  // Create reference to resolve test_waiter
+  let test_waiter_resolver;
+  // Create promise to hold test start until transaction is ready
+  test_waiter = new Promise<void>((resolve) => {
+    // Set resolver
+    test_waiter_resolver = resolve;
+  });
+
+  // Start client transaction
+  tx_runner = p_client.$transaction(async (tx) => {
+    // Unbind DI DB_CLIENT
+    await iocContainer.unbind("DB_CLIENT");
+    // Bind DI DB_CLIENT to transaction
+    // IMPORTANT: Due to DI cannot run tests in parallel (since DB_CLIENT is rebound repeatedly)
+    iocContainer
+      .bind("DB_CLIENT")
+      .toDynamicValue(() => tx)
+      .inTransientScope();
+
+    // Resolve test_waiter (transaction ready)
+    test_waiter_resolver();
+
+    // Wait for test to run
+    await tx_waiter;
+
+    // Force rollback
+    throw new Rollback();
+  });
+
+  // Wait for transaction to be ready
+  await test_waiter;
+});
+
+afterEach(async () => {
+  // Close transaction
+  tx_close();
+
+  try {
+    // Get return from transaction
+    await tx_runner;
+  } catch (e) {
+    // Catch non-rollbacks
+    if (!(e instanceof Rollback)) throw e;
+  }
+});
 
 afterAll(async () => {
   // Drop container
--- apps/backend/tests/integration/user-controller.test.ts
+++ apps/backend/tests/integration/user-controller.test.ts
@@ -2,85 +2,47 @@ import { expect, test } from "vitest";
 import app from "../../src/app";
 import request = require("supertest");
 import { UserCreationProps } from "../../src/controllers/user-controller";
-import { PrismaClient, User } from "@prisma/client";
-import config from "../../src/config/config";
+import { User, Prisma } from "@prisma/client";
 import { iocContainer } from "../../src/ioc";
-import { Rollback } from "../../src/types/exceptions";
 
 test("POST /user creates a user and returns 201", async () => {
-  const p_client: PrismaClient = new PrismaClient({
-    datasourceUrl: config.db_url,
+  const db_client: Prisma.TransactionClient = iocContainer.get("DB_CLIENT");
+
+  // Create user
+  const res = await request(app)
+    .post("/user")
+    .send({ username: "bilaal", password: "test" } as UserCreationProps);
+  expect(res.status).toBe(201);
+
+  // Assert user returned
+  const parsed_body = <User>res.body;
+  expect(parsed_body.username).toBe("bilaal");
+  expect(parsed_body).toHaveProperty("password");
+  expect(parsed_body).toHaveProperty("id");
+
+  // Check user inserted into database
+  const user = await db_client.user.findUnique({
+    where: { username: "bilaal" },
   });
-
-  try {
-    await p_client.$transaction(async (tx) => {
-      await iocContainer.unbind("DB_CLIENT");
-
-      iocContainer
-        .bind("DB_CLIENT")
-        .toDynamicValue(() => tx)
-        .inTransientScope();
-
-      // Create user
-      const res = await request(app)
-        .post("/user")
-        .send({ username: "bilaal", password: "test" } as UserCreationProps);
-      expect(res.status).toBe(201);
-
-      // Assert user returned
-      const parsed_body = <User>res.body;
-      expect(parsed_body.username).toBe("bilaal");
-      expect(parsed_body).toHaveProperty("password");
-      expect(parsed_body).toHaveProperty("id");
-
-
-      // Check user inserted into database
-      const user = await tx.user.findUnique({
-        where: { username: "bilaal" },
-      });
-      expect(user.username).toBe("bilaal");
-
-      throw new Rollback();
-    });
-  } catch (e) {
-    if (e instanceof Rollback) return;
-    throw e;
-  }
+  expect(user.username).toBe("bilaal");
 });
 
 test("POST /user with existing username returns 409", async () => {
-  const p_client: PrismaClient = new PrismaClient({
-    datasourceUrl: config.db_url,
+  // Setup database client
+  const db_client: Prisma.TransactionClient = iocContainer.get("DB_CLIENT");
+
+  // Insert mock user
+  await db_client.user.create({
+    data: {
+      username: "bilaal",
+      password: "test",
+    },
   });
 
-  try {
-    await p_client.$transaction(async (tx) => {
-      await iocContainer.unbind("DB_CLIENT");
-
-      iocContainer
-        .bind("DB_CLIENT")
-        .toDynamicValue(() => tx)
-        .inTransientScope();
-
-      // Insert mock user
-      await tx.user.create({
-        data: {
-          username: "bilaal",
-          password: "test",
-        },
-      });
-
-      // Create user with existing username
-      const res = await request(app)
-        .post("/user")
-        .send({ username: "bilaal", password: "test" } as UserCreationProps);
-      expect(res.status).toBe(409);
-      expect(res.body.message).toBe("Username already exists");
-
-      throw new Rollback();
-    });
-  } catch (e) {
-    if (e instanceof Rollback) return;
-    throw e;
-  }
+  // Create user with existing username
+  const res = await request(app)
+    .post("/user")
+    .send({ username: "bilaal", password: "test" } as UserCreationProps);
+  expect(res.status).toBe(409);
+  expect(res.body.message).toBe("Username already exists");
 });
