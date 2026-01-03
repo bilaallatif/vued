13ba4d2814d369214154798bb3d82c2724e2caea
bilaallatif <latif.bilaal@outlook.com>
2026-01-03 21:17:35 +0000

    test: added test covering 'GET /profile/me'

--- apps/backend/tests/integration/profile-controller.test.ts
+++ apps/backend/tests/integration/profile-controller.test.ts
@@ -41,3 +41,38 @@ test("GET /profile returns list of profiles", async () => {
     },
   ]);
 });
+
+test("GET /profile/me returns the requester users profile", async () => {
+  const db_client: Prisma.TransactionClient = iocContainer.get("DB_CLIENT");
+
+  // Mock users with associated profiles
+  const mock_user = await db_client.user.create({
+    data: {
+      username: "bilaal",
+      password: "test",
+      profile: {
+        create: {
+          bio: "Edit your bio!",
+        },
+      },
+    },
+  });
+
+  // Generate access token for user
+  const access_token = sign({ userId: mock_user.id }, "test", {
+    expiresIn: "60s",
+  });
+
+  const res = await request(app)
+    .get("/profile/me")
+    .set("Authorization", `Bearer ${access_token}`);
+
+  expect(res.statusCode).toBe(200);
+
+  expect(res.body).toEqual({
+    user: {
+      username: mock_user.username,
+    },
+    bio: "Edit your bio!",
+  });
+});
