f652550e16df62f76fe45e496ef5848455e96c5e
bilaallatif <latif.bilaal@outlook.com>
2026-01-03 21:27:01 +0000

    feat: implemented basic profile page displaying existing profile information

--- apps/frontend/src/routers/routers/authenticated_routers/profile.tsx
+++ apps/frontend/src/routers/routers/authenticated_routers/profile.tsx
@@ -1,10 +1,35 @@
 import { createRoute } from "@tanstack/react-router";
 import { AuthenticatedLayoutRoute } from "../authenticated_base.tsx";
+import { FormInput } from "../../../components/forms.tsx";
+import { BasicButton } from "../../../components/buttons.tsx";
+import { useEffect, useState } from "react";
+import { Default } from "@vued/sdk/api";
 
 const Profile = () => {
+  const [bio, setBio] = useState<string>("");
+
+  const populateProfileDetails = async () => {
+    const profiles_data = await Default.getMyProfile();
+    if (!profiles_data.error && profiles_data.data) {
+      setBio(profiles_data.data.bio);
+    }
+  };
+
+  useEffect(() => {
+    populateProfileDetails();
+  }, []);
+
   return (
     <div className={"w-full h-full flex flex-col items-center justify-center"}>
-      <h1 className={"text-5xl text-neutral-400"}>Profile</h1>
+      <div className={"flex flex-col px-10 w-full lg:px-0 lg:w-1/4 gap-10"}>
+        <div className={"flex flex-col w-full gap-2"}>
+          <FormInput name={"Username"} value={bio} setValue={setBio} />
+        </div>
+
+        <div className={"flex flex-row justify-start w-full gap-5"}>
+          <BasicButton text={"Update"} />
+        </div>
+      </div>
     </div>
   );
 };
