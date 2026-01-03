569d0c2bc7df82603178836ddc15a8e5a76b05d3
bilaallatif <latif.bilaal@outlook.com>
2026-01-03 20:36:21 +0000

    feat: implemented basic users(profiles) page

--- apps/frontend/src/routers/routers/authenticated_routers/users.tsx
+++ apps/frontend/src/routers/routers/authenticated_routers/users.tsx
@@ -1,10 +1,48 @@
 import { createRoute } from "@tanstack/react-router";
 import { AuthenticatedLayoutRoute } from "../authenticated_base.tsx";
+import { List } from "../../../components/list.tsx";
+import { Card } from "../../../components/card.tsx";
+import { useEffect, useState } from "react";
+import { Default, type ProfileDetailsDto } from "@vued/sdk/api";
+
+const UserCard = ({ username, bio }: { username: string; bio: string }) => {
+  return (
+    <Card>
+      <div className={"flex flex-col"}>
+        <div className={"text-3xl"}>{username}</div>
+        <div className={"text-xl"}>{bio}</div>
+      </div>
+    </Card>
+  );
+};
+
+const UsersList = () => {
+  const [profiles, setProfiles] = useState<ProfileDetailsDto[]>([]);
+
+  const populateProfiles = async () => {
+    const profiles_data = await Default.getProfiles();
+    if (!profiles_data.error && profiles_data.data) {
+      setProfiles(profiles_data.data);
+    }
+  };
+
+  useEffect(() => {
+    populateProfiles();
+  }, []);
+
+  return (
+    <List>
+      {profiles.map((profile) => (
+        <UserCard username={profile.user.username} bio={profile.bio} />
+      ))}
+    </List>
+  );
+};
 
 const Users = () => {
   return (
-    <div className={"w-full h-full flex flex-col items-center justify-center"}>
-      <h1 className={"text-5xl text-neutral-400"}>Users</h1>
+    <div className={"w-full h-full flex flex-col items-center"}>
+      <UsersList />
     </div>
   );
 };
