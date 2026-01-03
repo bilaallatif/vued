6e21ab5511c6897a4b1adb1eb831334792c2f626
bilaallatif <latif.bilaal@outlook.com>
2026-01-03 21:38:20 +0000

    test: added integration test covering 'PATCH /profile/me'

--- apps/backend/tests/integration/profile-controller.test.ts
+++ apps/backend/tests/integration/profile-controller.test.ts
@@ -76,3 +76,39 @@ test("GET /profile/me returns the requester users profile", async () => {
     bio: "Edit your bio!",
   });
 });
+
+test("PATCH /profile updates the requester users profile", async () => {
+  const db_client: Prisma.TransactionClient = iocContainer.get("DB_CLIENT");
+
+  // Mock users with associated profiles
+  const mock_user = await db_client.user.create({
+    data: {
+      username: "bilaal",
+      password: "test",
+      profile: {
+        create: {
+          bio: "Edit your bio!",
+        },
+      },
+    },
+  });
+
+  // Generate access token for user
+  const access_token = sign({ userId: mock_user.id }, "test", {
+    expiresIn: "60s",
+  });
+
+  const res = await request(app)
+    .patch("/profile/me")
+    .send({ bio: "New bio!" })
+    .set("Authorization", `Bearer ${access_token}`);
+
+  expect(res.statusCode).toBe(200);
+
+  expect(res.body).toEqual({
+    user: {
+      username: mock_user.username,
+    },
+    bio: "New bio!",
+  });
+});
